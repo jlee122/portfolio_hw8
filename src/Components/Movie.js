8c8afef4ad236fb6b52c8d30635cc8398621fbdf
jlee122 <jlee11@umail.ucsb.edu>
2020-05-17 21:04:46 -0700

    working

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -2,35 +2,52 @@ import React, { Component } from 'react'
 import { SRLWrapper } from "simple-react-lightbox"
 import ScrollUpButton from "react-scroll-up-button"
 
-const IDs = [ 'tt7286456', 'tt6751688']
+const IDs = [ 'tt7286456', 'tt4154796']
 const axios = require('axios')
 
 const PREFIX = 'http://www.omdbapi.com/?apikey=ec83e25b&i='
 
-async function getMoviePoster(movie){
-    const res =await axios.get(PREFIX+movie);
-    const data = await res.data;
-    return data.Poster
+function getMoviePoster(movie){
+    axios.get(PREFIX+movie).then((res)=>{
+        const data = res.data;
+        var x = document.getElementById(movie)
+        x.src = data["Poster"].toString()
+        return
+    })
+    .catch((e) => {
+        console.log(e);
+    })
+      
 }
 
 export class Movie extends Component {
     constructor() {
         super();
-        this.movies = [ 'tt7286456', 'tt6751688', 'tt4154796', 'tt0816692', 'tt1375666', 'tt1706620', 'tt3501632', 'tt0993846', 'tt4154756', 'tt5463162']
+        this.state = {
+            movies: [ 'tt7286456', 'tt4154796', 'tt0816692', 'tt1375666', 'tt1706620', 'tt3501632', 'tt0993846', 'tt4154756', 'tt5463162']
+        };
     }
 
     render() {
-        return <div className="movie-grid">
-            <div className="movie">
-                {this.movies.map((id) => {
-                    
-                    console.log(getMoviePoster(id))
-                    return (
-                        <img src={getMoviePoster(id)}></img>
-                    )
-                })}
+        return (
+            <SRLWrapper>
+            <div className="movie-grid">
+                <div className="movie">
+                    {this.state.movies.map((id) => {  
+                        getMoviePoster(id)                      
+                        return (
+                            <img id={id}></img>
+                            // <li key={id}>
+                            //     {console.log(id)}                                
+                            //     <img src={getMoviePoster(id)}></img>
+                            // </li>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+            <ScrollUpButton />
+            </SRLWrapper>
+        )
     }
 }
 export default Movie;
