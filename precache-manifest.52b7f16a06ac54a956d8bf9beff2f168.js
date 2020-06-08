ea10718c9c9b5f120625d427db7417c8c5dbc52b
jlee122 <jlee11@umail.ucsb.edu>
2020-06-07 22:58:36 -0700

    Updates

---
+++ precache-manifest.52b7f16a06ac54a956d8bf9beff2f168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089013f87fca76208cefdb9ff03bd5ee",
+    "url": "/portfolio_hw8/index.html"
+  },
+  {
+    "revision": "fe714c8ffbbf710bd6b1",
+    "url": "/portfolio_hw8/static/css/2.8091059e.chunk.css"
+  },
+  {
+    "revision": "74b07b168b58a1492980",
+    "url": "/portfolio_hw8/static/css/main.d98e9520.chunk.css"
+  },
+  {
+    "revision": "fe714c8ffbbf710bd6b1",
+    "url": "/portfolio_hw8/static/js/2.e7d23bcd.chunk.js"
+  },
+  {
+    "revision": "c8a852a4ab008bbcc3a1f8f927137104",
+    "url": "/portfolio_hw8/static/js/2.e7d23bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b07b168b58a1492980",
+    "url": "/portfolio_hw8/static/js/main.228fa2c1.chunk.js"
+  },
+  {
+    "revision": "23d1b2b8c49f0e16b529",
+    "url": "/portfolio_hw8/static/js/runtime-main.349bef62.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio_hw7/precache-manifest.6afa77d6bbbccca7fc15975c756bfd1e.js"
+  "/portfolio_hw8/precache-manifest.52b7f16a06ac54a956d8bf9beff2f168.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_hw7/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_hw8/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
